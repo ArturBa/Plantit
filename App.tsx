6979c641e447a2d0056626c63f5fa7abc1ba74e8
Artur Bauer <bauerart1@gmail.com>
2022-06-16 13:35:29 +0200

    feat(home screen): :recycle: replace modals with cards to make the experience as expected on iphone

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ export default function App() {
     return null;
   }
 
-  const statusBarStyle: StatusBarStyle = 'inverted';
+  const statusBarStyle: StatusBarStyle = 'dark';
 
   return (
     <ProviderCombo>
