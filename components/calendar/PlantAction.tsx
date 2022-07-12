7105ad1be5420c95ce4e6364f9fbe68c25ad9550
Artur Bauer <bauerart1@gmail.com>
2022-07-12 18:44:29 +0200

    feat(calendar): :lipstick: update calendar todo view

--- components/calendar/PlantAction.tsx
+++ components/calendar/PlantAction.tsx
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 4,
+    padding: 8,
   },
   iconWrapper: {
     backgroundColor: colors.accentBasic,
